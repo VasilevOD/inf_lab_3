da578838ed2345c51f8c6d44c95692a5cc0cd3fb
VasilevOD <121363280+VasilevOD@users.noreply.github.com>
2023-06-29 19:42:35 +0300

    3_2.c

--- 3_2.c
+++ 3_2.c
@@ -1,29 +1,57 @@
-#include<stdio.h>
+#include <stdio.h>
 
-
-void expand(char s1[], char s2[])
-{
-    int i, j, k;
-    k = 0;
-    for (i = 0; s1[i] != '\0'; i++)
-        if (s1[i] == '-')
-            if (i == 0 || s1[i+1] == '\0')
-            s2[k++] = '-';
-            else
-                for (j = s1[i-1]; j <= s1[i+1]; j++)
-                    s2[k++] = j;
-            s2[k] = '\0';
-}
- 
-int main(void)
-{
-    int ku; 
-    while ((ku=getchar())!='\n'){
-        
+void del(char *s1, char const *s2) {
+    int amount1 = 0;
+    for (int i = 0; s1[i] != '\0'; i++) {
+        amount1++;
     }
-    char s1[] = "-a-z0-9d-f-";
-    char s2[128] = "";
-    expand(s1, s2);
-    printf("s1=%s\ns2=%s\n", s1, s2);
+    
+    for (int i = 0; s1[i] != '\0';) {
+        int j;
+        for (j = 0; s2[j] != '\0'; j++) {
+            if (s1[i] == s2[j]) {
+                s1[i] = s1[amount1-1];
+                s1[amount1-1] = "\0";
+                amount1--;
+                break;
+            }
+        }
+        if (s2[j] == 0) {
+            i++;
+        }
+    }
+}
+
+int main () {
+    char s1[100];
+    char s2[100];
+    char s1_letters;
+    char s2_letters;
+    int amount1 = 0;
+    int amount2 = 0;
+    
+    while (s1_letters = getchar()) {
+        if (s1 != '\n') {
+            s1[amount1] = s1_letters;
+            amount1++;
+        }
+        else {
+            s1[amount1] = '\0';
+            break;
+        }
+    }
+
+    while (s2_letters = getchar()) {
+        if (s2 != '\n') {
+            s2[amount2] = s2_letters;
+            amount2++;
+        }
+        else {
+            s2[amount2] = '\0';
+            break;
+        }
+    }
+    del(s1, s2);
+    printf("%s\n", s1);
     return 0;
 }
