a6b7dd50bcb821bca2f9069c0b74acbdf69c197a
VasilevOD <121363280+VasilevOD@users.noreply.github.com>
2023-06-29 19:41:28 +0300

    3_1.c

--- 3_1.c
+++ 3_1.c
@@ -1,53 +1,42 @@
 #include <stdio.h>
 #include <ctype.h>
-#define MAX 500
-void expand (char *s1, char *s2);
-int main(){
-    
-  for(i = 0; i < 10; i++) {
-	scanf("%s", &z);
-	s1[i] = z;
-  }
-  char s1[MAX];
-  char s2[MAX];
-  s1[MAX]=gets();
-  expand(s1,s2);
-  printf("%s\n", s2);
-  return 0;
+#define FREE (0)
+int main() {
+    char i;
+    char start = FREE;
+    char end;
+    int direction;
+    int space = 1;
+    while((i = getchar()) != 0) {
+       if (isalnum(i) != 0) {
+           if (start == FREE) {
+               start = i;
+           }
+           else {
+               end = i;
+               if (start - end < 0) {
+                   direction = 1;
+               }
+               else {
+                   direction = -1;
+               }
+               for (i = start; i != end; i += direction) {
+                   putchar(i);
+               }
+               i = getchar();
+               if (i == '-') {
+                   start = end;
+               }
+               else {
+                   putchar(end);
+                   start = (isalnum(i)) ? i : FREE;
+               }
+           }
+       }
+       else if (start == FREE) {
+           putchar(i);
+       }
+    }
 }
-void expand (char *s1, char *s2)
-{
-  int i,j;
-  int k=0;
-  for (i=j=0; s1[i] != '\0'; i++){
-      if(s1[i] == ' '){
-          s2[k++] = ' ';
-            }
-      else if(s1[i] == '-'){
-          s2[k++] = '-';
-        }
-      else{
-          for(j=s1[i]; j<=s1[i+2]; j++){
-                  s2[k++]=j;
-            }
- 
-          i+=2;
-          if(s1[i+1]=='-' && isalnum(s1[i+2])){
-              for(j=s1[i]; s1[i]!= ' '; i++){
-                  if(s1[i]=='-'){
-                      i++;
-                    }
-                  else{
-                      for(--k; s1[i+1]!=' '; i++){
-                          ;
-                        }
-                      while(j<=s1[i]){
-                          s2[k++]=j++;
-                        }
-                      s2[k++]=' ';
-                    }
-                }
-            }
-        }
     }
 } 
